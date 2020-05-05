74d55a5ba1ad127a8606ec9a48c8d75434cad8cf
Gabriel Stratan <contact@ithanium.org>
2020-05-05 22:27:28 +0100

    fixed angular application routing

--- myangular-app/src/app/app-routing.module.ts
+++ myangular-app/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
+import { DefaultComponent } from './default/default.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '**',
+    component: DefaultComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- start-composition.sh
+++ start-composition.sh
@@ -1,1 +1,4 @@
-docker-compose up --build
+#docker-compose build --no-cache
+#docker-compose up --build
+docker-compose build --force-rm --no-cache && docker-compose up --detach
+docker-compose logs -f
